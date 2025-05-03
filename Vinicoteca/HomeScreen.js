639a0ff5f3c9e5558383dc4dd848835f6ce1111b
BMateusSs <bmdss0810@gmail.com>
2025-05-03 17:00:59 -0300

    Menu inferior

--- Vinicoteca/HomeScreen.js
+++ Vinicoteca/HomeScreen.js
@@ -10,6 +10,7 @@ import {
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import styles from "./StyleHome";
+import BottomMenu from "./ButtonMenu";
 import vinhosData from './vinhos.json';
 
 const Home = () => {
@@ -120,34 +121,7 @@ const Home = () => {
         </View>
       </ScrollView>
 
-      {/* Bottom Navigation Menu */}
-      <View style={[
-        styles.bottomMenu,
-        { 
-          paddingBottom: insets.bottom,
-          height: 60 + insets.bottom 
-        }
-      ]}>
-        <TouchableOpacity style={styles.menuItem}>
-          <MaterialCommunityIcons name="home" size={26} color="#6B2737" />
-          <Text style={styles.menuText}>Início</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.menuItem}>
-          <MaterialCommunityIcons name="glass-wine" size={26} color="#6B2737" />
-          <Text style={styles.menuText}>Biblioteca</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.menuItem}>
-          <MaterialCommunityIcons name="bottle-wine" size={26} color="#6B2737" />
-          <Text style={styles.menuText}>Minha Adega</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.menuItem}>
-          <MaterialCommunityIcons name="store" size={26} color="#6B2737" />
-          <Text style={styles.menuText}>Loja</Text>
-        </TouchableOpacity>
-      </View>
+      <BottomMenu/>
     </View>
   );
 };
