49695a8954d78f86b10eb15e34b22e941522de1a
BMateusSs <bmdss0810@gmail.com>
2025-05-04 19:13:22 -0300

    Separa validador de senha

--- src/screens/auth/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen.tsx
@@ -1,97 +1,58 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./Login";
 import { ConfirmButton } from "../../components/ConfirmButton";
 import { InputEmail } from "../../components/InputEmail";
 import CreatePassword from "../../components/CreatePassword";
 import Link from "../../components/Link";
-import { InputPassword } from "../../components/InputPassword";
+import { SignUpValidation } from "../../hooks/SignUpValidation";
 
-export default function SignUp(){
-    const navigation = useNavigation()
-    const [email, setEmail] = useState("");
+export default function SignUp() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  
+  const { errorMessage, validateSignUp } = SignUpValidation();
 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const handleSignUp = () => {
-    
-      if (password !== confirmPassword) {
-        setErrorMessage('As senhas não coincidem');
-        return;
-      }
-    
-      if (password.length < 6) {
-        setErrorMessage('A senha deve ter pelo menos 6 caracteres');
-        return;
-      }
-    
-      const hasUpperCase = /[A-Z]/.test(password);
-      const hasLowerCase = /[a-z]/.test(password);
-      const hasNumber = /[0-9]/.test(password);
-      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    
-      if (!hasUpperCase) {
-        setErrorMessage('A senha deve conter pelo menos uma letra maiúscula');
-        return;
-      }
-    
-      if (!hasLowerCase) {
-        setErrorMessage('A senha deve conter pelo menos uma letra minúscula');
-        return;
-      }
-    
-      if (!hasNumber) {
-        setErrorMessage('A senha deve conter pelo menos um número');
-        return;
-      }
-    
-      if (!hasSpecialChar) {
-        setErrorMessage('A senha deve conter pelo menos um caractere especial (!@#$%^&*(),.?":{}|<>)');
-        return;
-      }
-    
-      setErrorMessage('');
+  const handleSignUp = () => {
+    if (validateSignUp(password, confirmPassword)) {
       console.log('Usuário cadastrado com sucesso!');
-    };
+    }
+  };
 
-    return(
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.title}>Cadastrar</Text>
-            </View>
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Cadastrar</Text>
+      </View>
 
-            <TextInput
-            style={styles.containerInput}
-            placeholder="Informe seu nome" 
-            />
+      <TextInput
+        style={styles.containerInput}
+        placeholder="Informe seu nome" 
+      />
 
-            <InputEmail
-                email={email}
-                onChangeEmail={setEmail}
-                hasError={false}
-            />
-            
+      <InputEmail
+        email={email}
+        onChangeEmail={setEmail}
+        hasError={false}
+      />
+      
+      <CreatePassword 
+        password={password}
+        setPassword={setPassword}
+        confirmPassword={confirmPassword}
+        setConfirmPassword={setConfirmPassword}
+        errorMessage={errorMessage}
+      />
 
-            <CreatePassword 
-                password={password}
-                setPassword={setPassword}
-                confirmPassword={confirmPassword}
-                setConfirmPassword={setConfirmPassword}
-                errorMessage={errorMessage}
-                />
+      <ConfirmButton
+        title="Registrar"
+        onPress={handleSignUp}
+      />
 
-            <ConfirmButton
-                title="Registrar"
-                onPress={handleSignUp}
-            />
-
-            <Link to="Login" label="Já tem uma conta? *Entrar*"/>
-
-        </View>
-    )
+      <Link to="Login" label="Já tem uma conta? *Entrar*"/>
+    </View>
+  );
 }
-
-
