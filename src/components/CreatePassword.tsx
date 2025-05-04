f90044301efea05b97d635a56c1c7e9d0afbdd86
BMateusSs <bmdss0810@gmail.com>
2025-05-04 18:07:33 -0300

    Visualização de senha

--- src/components/CreatePassword.tsx
+++ src/components/CreatePassword.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface CreatePasswordProps {
   password: string;
@@ -16,45 +17,64 @@ const CreatePassword: React.FC<CreatePasswordProps> = ({
   setConfirmPassword,
   errorMessage
 }) => {
+
+    const [visible, setVisible] = useState(true)
+
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
+    <View >
+        <View style={styleInputPassword.container}>
+            <TextInput
+            style={styleInputPassword.input}
+            placeholder="Senha"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={!visible}/>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Confirmar senha"
-        value={confirmPassword}
-        onChangeText={setConfirmPassword}
-        secureTextEntry
-      />
+            <TouchableOpacity onPress={() => setVisible((v) => !v)}>
+                <Icon name={visible ? 'eye-off' : 'eye'} size={24} color="gray" />
+            </TouchableOpacity>
+        </View>
+      
+        <View style={styleInputPassword.container}>
+            <TextInput
+            style={styleInputPassword.input}
+            placeholder="Confirmar senha"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry/>
 
-      {errorMessage && <Text style={styles.errorText}>{errorMessage}</Text>}
+            <TouchableOpacity onPress={() => setVisible((v) => !v)}>
+                <Icon name={visible ? 'eye-off' : 'eye'} size={24} color="gray" />
+            </TouchableOpacity>
+            
+        </View>
+      
+
+      {errorMessage && <Text>{errorMessage}</Text>}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    marginVertical: 10,
-  },
-  input: {
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  },
-  errorText: {
-    color: 'red',
-    fontSize: 12,
-  }
-});
+const styleInputPassword = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 20,
+      minWidth: '90%',
+    },
+    input: {
+      flex: 1,
+      
+      height: 40,
+      marginRight: 10,
+      borderColor: '#fff',
+    },
+  });
+
+
 
 export default CreatePassword;
